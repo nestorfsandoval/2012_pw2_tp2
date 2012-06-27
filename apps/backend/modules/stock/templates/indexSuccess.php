67d9d5cb35ac34f78824f92261170981ffa1fe35
Nestor <nestor@HP-Pavilion-dv5-2133.(none)>
2012-06-26 23:45:23 -0300

    mas arreglos en INTRANET

--- apps/backend/modules/stock/templates/indexSuccess.php
+++ apps/backend/modules/stock/templates/indexSuccess.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="cuerpo">
 <fieldset>
         <legend>Control de Stock</legend>
@@ -35,37 +34,3 @@
   
 </fieldset>	
 </div>	
-=======
-<h1>Productos List</h1>
-
-<table>
-  <thead>
-    <tr>
-      <th>Idproducto</th>
-      <th>Titulo</th>
-      <th>Idartista</th>
-      <th>Idgenero</th>
-      <th>Anio</th>
-      <th>Cantidadventas</th>
-      <th>Stock</th>
-      <th>Precio</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($productos as $producto): ?>
-    <tr>
-      <td><a href="<?php echo url_for('stock/edit?idproducto='.$producto->getIdproducto()) ?>"><?php echo $producto->getIdproducto() ?></a></td>
-      <td><?php echo $producto->getTitulo() ?></td>
-      <td><?php echo $producto->getIdartista() ?></td>
-      <td><?php echo $producto->getIdgenero() ?></td>
-      <td><?php echo $producto->getAnio() ?></td>
-      <td><?php echo $producto->getCantidadventas() ?></td>
-      <td><?php echo $producto->getStock() ?></td>
-      <td><?php echo $producto->getPrecio() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
-
-  <a href="<?php echo url_for('stock/new') ?>">New</a>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
--- apps/backend/modules/usuarios/templates/_form.php
+++ apps/backend/modules/usuarios/templates/_form.php
@@ -10,107 +10,67 @@
       <tr>
         <td colspan="2">
           <?php echo $form->renderHiddenFields(false) ?>
-<<<<<<< HEAD
           &nbsp;<a href="<?php echo url_for('usuarios/index') ?>">Volver</a>
           <?php if (!$form->getObject()->isNew()): ?>
             &nbsp;<?php echo link_to('Borrar', 'usuarios/delete?id_emp='.$form->getObject()->getIdEmp(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
           <?php endif; ?>
           <input type="submit" value="Guardar" />
-=======
-          &nbsp;<a href="<?php echo url_for('usuarios/index') ?>">Back to list</a>
-          <?php if (!$form->getObject()->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'usuarios/delete?id_emp='.$form->getObject()->getIdEmp(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-          <?php endif; ?>
-          <input type="submit" value="Save" />
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         </td>
       </tr>
     </tfoot>
     <tbody>
       <?php echo $form->renderGlobalErrors() ?>
       <tr>
-<<<<<<< HEAD
         <th><?php echo $form['apellido']->renderLabel('Apellido') ?></th>
-=======
-        <th><?php echo $form['apellido']->renderLabel() ?></th>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         <td>
           <?php echo $form['apellido']->renderError() ?>
           <?php echo $form['apellido'] ?>
         </td>
       </tr>
       <tr>
-<<<<<<< HEAD
         <th><?php echo $form['nombre']->renderLabel('Nombre') ?></th>
-=======
-        <th><?php echo $form['nombre']->renderLabel() ?></th>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         <td>
           <?php echo $form['nombre']->renderError() ?>
           <?php echo $form['nombre'] ?>
         </td>
       </tr>
       <tr>
-<<<<<<< HEAD
         <th><?php echo $form['user']->renderLabel('Usuario') ?></th>
-=======
-        <th><?php echo $form['user']->renderLabel() ?></th>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         <td>
           <?php echo $form['user']->renderError() ?>
           <?php echo $form['user'] ?>
         </td>
       </tr>
       <tr>
-<<<<<<< HEAD
         <th><?php echo $form['pass']->renderLabel('Contrase&ntilde;a') ?></th>
-=======
-        <th><?php echo $form['pass']->renderLabel() ?></th>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         <td>
           <?php echo $form['pass']->renderError() ?>
           <?php echo $form['pass'] ?>
         </td>
       </tr>
       <tr>
-<<<<<<< HEAD
         <th><?php echo $form['mail']->renderLabel('Email') ?></th>
-=======
-        <th><?php echo $form['mail']->renderLabel() ?></th>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         <td>
           <?php echo $form['mail']->renderError() ?>
           <?php echo $form['mail'] ?>
         </td>
       </tr>
       <tr>
-<<<<<<< HEAD
         <th><?php echo $form['id_ciudad']->renderLabel('Ciudad') ?></th>
-=======
-        <th><?php echo $form['id_ciudad']->renderLabel() ?></th>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         <td>
           <?php echo $form['id_ciudad']->renderError() ?>
           <?php echo $form['id_ciudad'] ?>
         </td>
       </tr>
       <tr>
-<<<<<<< HEAD
         <th><?php echo $form['id_prov']->renderLabel('Provincia') ?></th>
-=======
-        <th><?php echo $form['id_prov']->renderLabel() ?></th>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         <td>
           <?php echo $form['id_prov']->renderError() ?>
           <?php echo $form['id_prov'] ?>
         </td>
       </tr>
       <tr>
-<<<<<<< HEAD
         <th><?php echo $form['idprivilegio']->renderLabel('Privilegio') ?></th>
-=======
-        <th><?php echo $form['idprivilegio']->renderLabel() ?></th>
->>>>>>> eedbe7e... acutalizacion de stock,compras en intranet
         <td>
           <?php echo $form['idprivilegio']->renderError() ?>
           <?php echo $form['idprivilegio'] ?>
