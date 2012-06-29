9e34172f918951033f5bd7b4449530373f965d5e
Luis Arrix <luis.arrix@gmail.com>
2012-06-29 16:06:59 -0300

    Agregada consulta para catalogo, toqueteado css y agregada imagen

--- apps/frontend/modules/catalogo/templates/indexSuccess.php
+++ apps/frontend/modules/catalogo/templates/indexSuccess.php
@@ -1,32 +1,29 @@
-<h1>Productos List</h1>
+<h3>Listado de productos</h3>
+
+<form id="left" action="<?php echo url_for('catalogo/index') ?>" method="GET">
+            <!-- ESTE INPUT OCULTO INDICA A QUE CONTENIDO DEBE MANDARLE LOS VALORES DEL FORMULARIO -->
+            <input type="hidden" name="op" value="3">
+            <input  class="campo" type="text" name="buscaProdu" placeholder="Ingrese Busqueda..." size="50" />
+            <input class="boton"type="image" name="buscar" src="<?php echo image_path('search_4.png')?>"/>
+</form>
 
 <table>
   <thead>
     <tr>
-      <th>Idproducto</th>
+      <th>Artista</th>
       <th>Titulo</th>
-      <th>Idartista</th>
-      <th>Idgenero</th>
-      <th>Anio</th>
-      <th>Ventas</th>
-      <th>Stock</th>
+      <th>Genero</th>
       <th>Precio</th>
     </tr>
   </thead>
   <tbody>
     <?php foreach ($productos as $producto): ?>
     <tr>
-      <td><a href="<?php echo url_for('catalogo/edit?idproducto='.$producto->getIdproducto()) ?>"><?php echo $producto->getIdproducto() ?></a></td>
-      <td><?php echo $producto->getTitulo() ?></td>
       <td><?php echo $producto->getIdartista() ?></td>
+      <td><?php echo $producto->getTitulo() ?></td>
       <td><?php echo $producto->getIdgenero() ?></td>
-      <td><?php echo $producto->getAnio() ?></td>
-      <td><?php echo $producto->getVentas() ?></td>
-      <td><?php echo $producto->getStock() ?></td>
-      <td><?php echo $producto->getPrecio() ?></td>
+      <td><?php echo '$'  .   $producto->getPrecio() ?></td> 
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
-
-  <a href="<?php echo url_for('catalogo/new') ?>">New</a>
